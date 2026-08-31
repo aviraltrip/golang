1b16f74705e15cfa28fcbc3ecacfb1a04d8f66a5
aviraltrip <aviraltripathi25@gmail.com>
2026-09-01 02:08:41 +0530

    get request

--- 21webreqserver/main.go
+++ 21webreqserver/main.go
@@ -1,5 +1,34 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	
+
+}
+
+func PerformGetRequest() {
+	const url = "http://localhost:3000/get"
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	fmt.Println("Status Code: ", res.StatusCode)
+	fmt.Println("Content Length is: ", res.ContentLength)
+
+	var resString strings.Builder
+	content, _ := io.ReadAll(res.Body)
+	byteCount, _ := resString.Write(content)
+
+	fmt.Println("ByteCount is: ", byteCount)
+	fmt.Println(resString.String())
+
+	fmt.Println(string(content))
+	fmt.Println(content)
+
 }
