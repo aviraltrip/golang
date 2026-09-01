3b18bf58178f6072179dabafa0af13ffa83dbffd
aviraltrip <aviraltripathi25@gmail.com>
2026-09-02 01:00:52 +0530

    post req made

--- 21webreqserver/main.go
+++ 21webreqserver/main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-
+	// PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
 func PerformGetRequest() {
@@ -18,17 +19,33 @@ func PerformGetRequest() {
 		panic(err)
 	}
 	defer res.Body.Close()
+
 	fmt.Println("Status Code: ", res.StatusCode)
 	fmt.Println("Content Length is: ", res.ContentLength)
 
 	var resString strings.Builder
 	content, _ := io.ReadAll(res.Body)
 	byteCount, _ := resString.Write(content)
-
 	fmt.Println("ByteCount is: ", byteCount)
 	fmt.Println(resString.String())
-
 	fmt.Println(string(content))
 	fmt.Println(content)
+}
 
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:3000/post"
+	// nakli json payload
+	requestBody := strings.NewReader(`
+	{
+		"coursename": "learning go",
+		"price": 0,
+		"platform": "https://aviraltrip.vercel.app/"
+	}`)
+	response, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
 }
