1454d6637901c4d6ece51a2f53767876ba43e79c
aviraltrip <aviraltripathi25@gmail.com>
2026-09-02 23:05:17 +0530

    Unmarshal decoding json

--- 22bitmorejson/main.go
+++ 22bitmorejson/main.go
@@ -14,7 +14,8 @@ type course struct {
 }
 
 func main() {
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -31,4 +32,33 @@ func EncodeJson() {
 		panic(err)
 	}
 	fmt.Printf("%s\n", finalJson)
+}
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"website": "LearnCodeOnline.in",
+		"tags": ["web-dev", "js"]
+	}
+`)
+	var lcoCourse course
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+	// some cases where just wanna add data to key value
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+	for k, v := range myOnlineData {
+		fmt.Printf("key is %v and value is %v and type is: %T\n", k, v, v)
+	}
+
 }
