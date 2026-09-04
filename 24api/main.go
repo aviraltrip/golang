83ffc47f996263ba668efe619198ea5ca12d80c0
aviraltrip <aviraltripathi25@gmail.com>
2026-09-04 23:13:03 +0530

    api

---
+++ 24api/main.go
@@ -0,0 +1,17 @@
+package main
+
+// Model for course file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+func main() {
+}
