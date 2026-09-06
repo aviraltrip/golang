b2fbf52e12e0efd5c812d9b8f834059d9dd30d95
aviraltrip <aviraltripathi25@gmail.com>
2026-09-07 03:38:04 +0530

    deleteonecourse

--- 24api/main.go
+++ 24api/main.go
@@ -113,3 +113,18 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode("No Course found with given id")
 }
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// loop, id, remove (index, index+1)
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+	}
+}
