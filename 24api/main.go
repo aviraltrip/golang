cce04e580a5296e8192140c0b36ea07f0409796c
aviraltrip <aviraltripathi25@gmail.com>
2026-09-07 15:56:55 +0530

    routing and seeding data

--- 24api/main.go
+++ 24api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -34,6 +35,24 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
+	fmt.Println("API Stuff")
+
+	// seeding
+	courses = append(courses, Course{CourseId: "2", CourseName: "ReactJS",
+		CoursePrice: 299, Author: &Author{Fullname: "Hitesh Choudhary", Website: "lco.dev"}})
+	courses = append(courses, Course{CourseId: "4", CourseName: "MERN Stack",
+		CoursePrice: 199, Author: &Author{Fullname: "Hitesh Choudhary", Website: "go.dev"}})
+
+	// routing
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // controllers-file
