2331b464489e09ede7865020f4c4eef42afb58ae
aviraltrip <aviraltripathi25@gmail.com>
2026-09-14 02:56:29 +0530

    wait groups in go

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,17 +2,46 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var wg sync.WaitGroup //pointer
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+	websitelist := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+	for _, web := range websitelist {
+		wg.Add(1)
+		go getStatusCode(web)
+	}
+	wg.Wait()
 }
 
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("Oops endpoint:", endpoint, err)
+		return
 	}
+	defer res.Body.Close()
+
+	fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
 }
