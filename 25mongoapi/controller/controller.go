5ca9625c0a22de3bb51d35885973c4505717298a
aviraltrip <aviraltripathi25@gmail.com>
2026-09-13 20:30:59 +0530

    actual controllers added

--- 25mongoapi/controller/controller.go
+++ 25mongoapi/controller/controller.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/aviraltrip/mongoapi/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -96,4 +98,12 @@ func getAllMovies() []primitive.M {
 
 	defer cur.Close(context.Background())
 	return movies
+}
+
+// actual controller - file
+
+func GetMyAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	allMovies := getAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
 }
