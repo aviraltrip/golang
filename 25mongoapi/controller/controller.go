a1474ca8fb028a20acba6254c51be49c357aff9d
aviraltrip <aviraltripathi25@gmail.com>
2026-09-13 15:48:20 +0530

    adding and update a record

--- 25mongoapi/controller/controller.go
+++ 25mongoapi/controller/controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aviraltrip/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -34,4 +37,26 @@ func init() {
 
 	//collection instance
 	fmt.Println("Collection instance is ready")
+}
+
+// mongodb helpers - file
+// insert 1 record
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted one movie in db with id: ", inserted.InsertedID)
+}
+
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	res, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("modified count: ", res.ModifiedCount)
 }
